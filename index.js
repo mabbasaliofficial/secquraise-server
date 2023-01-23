2dcb69d30a7699685505be2b1227bc08e9b6585c
mabbasaliofficial <mabbasaliofficial@gmail.com>
2023-01-23 17:08:18 +0600

    gender counter added

--- index.js
+++ index.js
@@ -17,37 +17,28 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("SecqurAise is available");
 });
 
-app.post("/create", (req, res) => {
-//   const data = req.body;
-//   db.collection("users")
-//     .add(data)
-//     .then((docRef) => {
-//       res.send({ message: "Data added successfully", id: docRef.id });
-//     })
-//     .catch((error) => {
-//       res.status(500).send({ message: "Error adding data", error });
-//     });
-    try{
-        const id = req.body.userId;
-        const user = {
-            id: id,
-            name: req.body.name,
-            image: req.body.image,
-            location: req.body.location,
-            gender: req.body.gender,
-            date: req.body.date,
-            time: req.body.time,
-        }
-        const response = db.collection('users').doc(id).set(user);
-        res.send(response);
-    }
-    catch(error) {
-        res.status(500).send({ message: "Error adding data", error });
-    }
+app.post("/create", async (req, res) => {
+  try {
+    const id = req.body.userId;
+    const user = {
+      id: id,
+      name: req.body.name,
+      image: req.body.image,
+      location: req.body.location,
+      gender: req.body.gender,
+      date: req.body.date,
+      time: req.body.time,
+    };
+    const response = db.collection("users").doc(id).set(user);
+    res.send(response);
+  } catch (error) {
+    res.status(500).send({ message: "Error adding data", error });
+  }
 });
 
 app.get("/users", async (req, res) => {
@@ -69,6 +60,34 @@ app.get("/users/:id", async (req, res) => {
     const userRef = db.collection("users").doc(req.params.id);
     const response = await userRef.get();
     res.send(response.data());
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/male", async (req, res) => {
+  try {
+    const usersRef = db.collection("users");
+    const response = await usersRef.where("gender", "==", "Male").get();
+    let responseArray = [];
+    response.forEach((doc) => {
+      responseArray.push(doc.data());
+    });
+    res.send(responseArray);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/female", async (req, res) => {
+  try {
+    const usersRef = db.collection("users");
+    const response = await usersRef.where("gender", "==", "Female").get();
+    let responseArray = [];
+    response.forEach((doc) => {
+      responseArray.push(doc.data());
+    });
+    res.send(responseArray);
   } catch (error) {
     res.send(error);
   }
